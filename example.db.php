d32b4c9091e74709302e67c406d56eb0fb36ea2f
kkirpichnikov <kkirpichnikov@gmail.com>
2021-08-07 08:49:04 -0400

    Added: gitignore

---
+++ example.db.php
@@ -0,0 +1,5 @@
+<?php
+$dbhost="127.0.0.1";
+$login="root";
+$password="mole";
+?>
---
+++ wegagui/config/example.conf.php
@@ -0,0 +1,8 @@
+<?php
+// Параметры базы
+
+include "../../db.php";
+$my_db="YOUR_DATABASE_NAME"; // Имя базы
+$tb="sens"; // Имя таблицы с данными
+
+?>
