f758f9da3339756252f75afd76390896df047588
mmatveyev <progl@progl.su>
2022-08-19 20:38:24 +0300

    common-installer

--- install.sh
+++ install.sh
@@ -8,8 +8,7 @@ read -p "[yes/no]: " OS_UPDATE
 WEGA_USER_NAME="wega"
 
 apt update
-if [[ "$OS_UPDATE" == 'yes' ]]
-then
+if [[ "$OS_UPDATE" == 'yes' ]]; then
   apt dist-upgrade -y
 fi
 
@@ -27,7 +26,7 @@ echo "##############################"
 printf "\n"
 sleep 5
 
-MYSQL=`which mysql`
+MYSQL=$(which mysql)
 MYSQL_PSWD=$(openssl rand -hex 12)
 
 Q1="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_PSWD';"
@@ -37,7 +36,7 @@ SQL="${Q1}${Q2}${Q3}"
 
 $MYSQL -uroot -e "$SQL"
 
-echo "log-bin-trust-function-creators = 1" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "log-bin-trust-function-creators = 1" >>/etc/mysql/mysql.conf.d/mysqld.cnf
 
 systemctl restart mysql
 
@@ -48,11 +47,13 @@ printf "\n"
 sleep 5
 
 git clone https://github.com/WEGA-project/WEGA.git /var/WEGA
+cd /var/WEGA
+git switch web-hpg
 
-# Syslog config 
+# Syslog config
 cp /var/WEGA/wegabox.conf /etc/syslog-ng/conf.d/wegabox.conf
-echo "password(\""$MYSQL_PSWD"\")" > /etc/syslog-ng/patterndb.d/dbpass
-mysql -uroot -p$MYSQL_PSWD < /var/WEGA/syslog.sql
+echo "password(\""$MYSQL_PSWD"\")" >/etc/syslog-ng/patterndb.d/dbpass
+mysql -uroot -p$MYSQL_PSWD </var/WEGA/syslog.sql
 
 systemctl restart syslog-ng
 
@@ -65,20 +66,62 @@ sleep 5
 ln -s /var/WEGA/apache/WEGA-auth.conf /etc/apache2/conf-enabled/
 ln -s /var/WEGA/apache/wega-api.conf /etc/apache2/conf-enabled/
 cp /var/WEGA/example.db.php /var/WEGA/db.php
-sed -i "s/DATABASE_PASSWORD/$MYSQL_PSWD/g" /var/WEGA/db.php 
+sed -i "s/DATABASE_PASSWORD/$MYSQL_PSWD/g" /var/WEGA/db.php
 
 chown -R www-data:www-data /var/WEGA
 
 # WEGA API secret config
 cp /var/WEGA/wega-api/wegabox.php.example /var/WEGA/wega-api/wegabox.php
 WEGA_API_TOKEN=$(openssl rand -hex 18)
-sed -i "s/adab637320e5c47624cdd15169276981/$WEGA_API_TOKEN/g"  /var/WEGA/wega-api/wegabox.php
-
+sed -i "s/adab637320e5c47624cdd15169276981/$WEGA_API_TOKEN/g" /var/WEGA/wega-api/wegabox.php
 
 # WEGA UI config
 WEGA_UI_PSWD=$(openssl rand -hex 10)
-cp /var/WEGA/wegagui/config/example/example.conf.php /var/WEGA/wegagui/config/esp32wega.conf.php 
+cp /var/WEGA/wegagui/config/example/example.conf.php /var/WEGA/wegagui/config/esp32wega.conf.php
 htpasswd -b -c /etc/apache2/.htpasswd $WEGA_USER_NAME $WEGA_UI_PSWD
+
+echo "###################################"
+echo "###### Install WEGA-WEB-HPG ######"
+echo "###################################"
+echo "Create database"
+cd /var/WEGA/wega-hpg/
+MYSQL=$(which mysql)
+DB_PASS=$(echo "<?php include '/var/WEGA/db.php'; echo \$password;" | /usr/bin/php)
+echo "Create wega-hpg db name: $MAIN_DB_NAME"
+SQL="CREATE DATABASE IF NOT EXISTS wegacalc;"
+$MYSQL -uroot -p$DB_PASS -e "$SQL"
+echo "Install python"
+a2enmod proxy_http
+apt-get install --yes --quiet --no-install-recommends python3-pip python3-venv build-essential libssl-dev libffi-dev python3-dev libmysqlclient-dev libev-dev python3-wheel
+deactivate
+rm -R /var/WEGA/wega-hpg/venv
+python3 -m venv /var/WEGA/wega-hpg/venv
+source /var/WEGA/wega-hpg/venv/bin/activate
+pip3 install wheel
+pip3 install -r /var/WEGA/wega-hpg/requirements.txt
+chmod 744 /var/WEGA/wega-hpg/entrypoint.sh
+cp /var/WEGA/wega-hpg/scripts/wega-hpg.service /etc/systemd/system/wega-hpg.service
+chmod 664 /etc/systemd/system/wega-hpg.service
+ln -s /var/WEGA/apache/wega-hpg.conf /etc/apache2/conf-enabled/
+mkdir -p /var/log/gunicron/
+systemctl daemon-reload
+systemctl enable wega-hpg.service
+systemctl restart wega-hpg.service
+a2enmod headers
+python manage_prod.py collectstatic --noinput
+python manage_prod.py migrate --noinput
+touch /var/WEGA/wega-hpg/WEGA_HPG_PASSWORD
+WEGA_HPG_PASSWORD=$(cat /var/WEGA/wega-hpg/WEGA_HPG_PASSWORD)
+if [[ -n "$WEGA_HPG_PASSWORD" ]]; then
+  echo "we already have it"
+else
+  echo "Generating new WEGA_HPG_PASSWORD"
+  WEGA_HPG_PASSWORD=$(openssl rand -hex 12)
+fi
+P_STRING="from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').delete(); User.objects.create_superuser('admin', 'admin@wega.ru', '$WEGA_HPG_PASSWORD')"
+echo $P_STRING | python manage_prod.py shell
+echo $WEGA_HPG_PASSWORD >/var/WEGA/wega-hpg/WEGA_HPG_PASSWORD
+echo "######  WEGA-WEB-HPG INSTALED ######"
 
 systemctl reload apache2
 
@@ -112,12 +155,11 @@ printf "\n\n"
 echo "#######################"
 echo "### WEGA SERVER URL ###"
 echo "#######################"
-if [[  "$(curl -s ipinfo.io | grep -o Amazon)" == "Amazon" ]]
-then
-    AWS_PUBLIC_IP=$(curl -s 'http://169.254.169.254/latest/meta-data/public-ipv4')
-    echo 'WEGA_SERVER_IP: http://'$AWS_PUBLIC_IP'/wega'
+if [[ "$(curl -s ipinfo.io | grep -o Amazon)" == "Amazon" ]]; then
+  AWS_PUBLIC_IP=$(curl -s 'http://169.254.169.254/latest/meta-data/public-ipv4')
+  echo 'WEGA_SERVER_IP: http://'$AWS_PUBLIC_IP'/wega'
 else
-    echo "WEGA_SERVER_IP: http://$(hostname -I | sed -e "s/\s$//g")/wega"
+  echo "WEGA_SERVER_IP: http://$(hostname -I | sed -e "s/\s$//g")/wega"
 fi
 
 echo "WEGA_UI_USERNAME: $WEGA_USER_NAME"
@@ -128,15 +170,18 @@ printf "\n"
 echo "######################"
 echo "### WEGABOX config ###"
 echo "######################"
-if [[  "$(curl -s ipinfo.io | grep -o Amazon)" == "Amazon" ]]
-then
-    AWS_PUBLIC_IP=$(curl -s 'http://169.254.169.254/latest/meta-data/public-ipv4')
-    echo 'String wegaapi  = "http://'$AWS_PUBLIC_IP'/wega-api/wegabox.php";'
-    echo '#define SYSLOG_SERVER "'$AWS_PUBLIC_IP'"'
+if [[ "$(curl -s ipinfo.io | grep -o Amazon)" == "Amazon" ]]; then
+  AWS_PUBLIC_IP=$(curl -s 'http://169.254.169.254/latest/meta-data/public-ipv4')
+  echo 'String wegaapi  = "http://'$AWS_PUBLIC_IP'/wega-api/wegabox.php";'
+  echo '#define SYSLOG_SERVER "'$AWS_PUBLIC_IP'"'
 else
-    SERVER_IP=$(hostname -I | sed -e "s/\s$//g"|awk '{print $1}')
-    echo 'String wegaapi  = "http://'$SERVER_IP'/wega-api/wegabox.php";'
-    echo '#define SYSLOG_SERVER "'$SERVER_IP'"'
+  SERVER_IP=$(hostname -I | sed -e "s/\s$//g" | awk '{print $1}')
+  echo 'String wegaapi  = "http://'$SERVER_IP'/wega-api/wegabox.php";'
+  echo '#define SYSLOG_SERVER "'$SERVER_IP'"'
 fi
 echo 'String wegaauth = "'$WEGA_API_TOKEN'";'
 echo 'String wegadb   = "esp32wega";'
+
+echo 'String WEGA-HPG user = 'admin@wega.ru''
+echo "String WEGA-HPG password   = '$WEGA_HPG_PASSWORD'"
+echo "WEGA-HPG: http://$(hostname -I | sed -e "s/\s$//g")/wega-hpg/"
