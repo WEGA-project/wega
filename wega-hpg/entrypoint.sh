09a1a810da2f007258b2a7725619eff3822e47bd
mmatveyev <progl@progl.su>
2022-08-19 15:08:55 +0300

    wega-calc

--- wega-hpg/entrypoint.sh
+++ wega-hpg/entrypoint.sh
@@ -1,5 +1,7 @@
 echo 'starting'
 set -xe
+cd /var/WEGA/wega-hpg/
+source /var/WEGA/wega-hpg/venv/bin/activate
 python manage_prod.py collectstatic --noinput
 python manage_prod.py migrate  --noinput
 python runserver.py --port 5003
--- wega-hpg/project/settings_prod.py
+++ wega-hpg/project/settings_prod.py
@@ -1,14 +1,26 @@
 from .settings import *
 DEBUG=False
-pwd = os.system("""$(echo "<?php include '/var/WEGA/db.php'; echo \$password;" | /usr/bin/php)""")
+def get_pwd(fn='/var/WEGA/db.php'):
+    with open(fn, 'r') as f:
+        lines = f.readlines()
+        for i  in lines:
+            try:
+                k, v = i.split('=')
+                if k=='$password':
+                    return v[1:-3]
+            except Exception as e:
+                pass
+                
+
 SECRET_KEY = '&9+%$$-s6g4@3y1%00x-4bg&i6=e(gupn27f0$e@*p&_y#n*u'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'wegacalc',
         'USER': 'root',
-        'PASSWORD': pwd,
+        'PASSWORD': get_pwd(),
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
+
