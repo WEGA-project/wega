711021e327b6059bcd55f12730cfeb234c62035e
Sergey Ivanov <siv237@gmail.com>
2022-08-22 16:57:20 +1000

    Исправлена ошибка автостарта графаны после ребута

--- grafana/install_ubuntu20.sh
+++ grafana/install_ubuntu20.sh
@@ -5,6 +5,9 @@ wget -q -O /usr/share/keyrings/grafana.key https://packages.grafana.com/gpg.key
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://packages.grafana.com/enterprise/deb stable main" > /etc/apt/sources.list.d/grafana.list
 apt update
 apt install -y grafana-enterprise
+systemctl enable grafana-server.service
+systemctl start grafana-server.service
 chown grafana:grafana /var/WEGA/grafana/dashboards -R
 cp /var/WEGA/grafana/wega-server.yaml /etc/grafana/provisioning/dashboards/
 bash datasource.sh
+
