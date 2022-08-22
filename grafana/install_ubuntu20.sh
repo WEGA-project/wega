65b55b860234badbc2bf018ab2c565a159b29aa7
Sergey Ivanov <siv237@gmail.com>
2022-08-22 10:54:02 +1000

    Добавлен скрипт установки grafana

---
+++ grafana/install_ubuntu20.sh
@@ -0,0 +1,6 @@
+apt install -y apt-transport-https
+apt install -y software-properties-common wget
+wget -q -O /usr/share/keyrings/grafana.key https://packages.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+apt update
+apt install -y grafana-enterprise
