4fe4c69cff3f373fc6ae225a99a7a7078bb79b2a
Sergey Ivanov <siv237@gmail.com>
2022-08-23 20:43:42 +1000

    Обновлен скрипт настройки syslog-ng сервера

--- syslogdb.sh
+++ syslogdb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt install syslog-ng libdbd-mysql -y
 dbpass=`cat db.php|grep "password="|awk -F '\"' '{print $2}'`
 echo "password(\""$dbpass"\")" > /etc/syslog-ng/patterndb.d/dbpass
 cp wegabox.conf /etc/syslog-ng/conf.d/wegabox.conf
